56d8ac66a1c2a261c5d80ee7b487b64fb151ca4c
pityfox <pityfox@gmail.com>
2016-09-30 12:07:49 +0200

    Guess Number

--- workspace/web/WebContent/guessNumber/guessNumber.js
+++ workspace/web/WebContent/guessNumber/guessNumber.js
@@ -1,3 +1,23 @@
 /**
  * 
  */
+var randomNb = Math.trunc(Math.random()*100);
+
+function guess(){
+	var essais = document.getElementsByName("tries")[0].value;
+	essais = parseInt(essais);
+	if(essais > 0){
+		var guessNb = document.getElementsByName("guess")[0].value;
+		guessNb = parseInt(guessNb);
+		if(guessNb > randomNb)
+			document.getElementById("reponse").innerHTML = "Trop grand";
+		else if(guessNb < randomNb)
+			document.getElementById("reponse").innerHTML = "Trop petit";
+		else
+			document.getElementById("reponse").innerHTML = "Gagné";
+		essais--;
+		document.getElementsByName("tries")[0].value = essais;
+	} else {
+		document.getElementById("reponse").innerHTML = "Perdu, c'éait "+randomNb;
+	}
+};
